9c908c6a203266540d0c5ddfa2001aa422f7d24e
juan ortega <jortegah79@gmail.com>
2024-08-16 17:01:46 +0200

    screens created and list barrel file updated

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,4 +1,9 @@
+export 'package:widgets_app/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_app/presentation/screens/app_tutorial/app_tutorial_screen.dart';
 export 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_app/presentation/screens/home/home_screen.dart';
-
+export 'package:widgets_app/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_app/presentation/screens/progress_screen/progress_screen.dart';
+export 'package:widgets_app/presentation/screens/snackbar_screen/snackbar_screen.dart';
+export 'package:widgets_app/presentation/screens/ui_controls/ui_controls_creen.dart';
